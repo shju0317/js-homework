80c1032db50eada4a6145bade9b5897801c647fc
shju0317 <shju0317@naver.com>
2023-07-16 20:25:47 +0900

    로그인 조건처리 수정

--- naver_login/js/main.js
+++ naver_login/js/main.js
@@ -62,10 +62,13 @@ pw.addEventListener('input',function(){
 })
 
 // 4. 로그인 버튼을 클릭시 조건처리
-button.addEventListener('click',()=>{
+button.addEventListener('click', e =>{
+   e.preventDefault();
+
   if(emailPass){
     if(email.value === user.id && pw.value === user.pw){
-      //window.location.href = 'welcome.html';
+      window.location.href = 'welcome.html';
+    
       console.log('로그인');
     }else{
       alert(' 아이디(로그인 전용 아이디) 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.');
