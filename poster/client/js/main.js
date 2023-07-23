2b0bb56ef057b4069ff6547117dc3d25b32497cc
shju0317 <shju0317@naver.com>
2023-07-23 21:17:51 +0900

    click한 li에 is-active 클래스 추가

--- poster/client/js/main.js
+++ poster/client/js/main.js
@@ -1,4 +1,4 @@
-import { attr, css, getNode } from '../lib/index.js'
+import { addClass, attr, css, getNode, getNodes, removeClass } from '../lib/index.js'
 
 /* 
 
@@ -14,7 +14,10 @@ const nav = getNode('.nav')
 const visual = getNode('.visual')
 const nickName = getNode('.nickName')
 const body = getNode('body');
+const li = getNodes('li');
 let index = 0;
+
+console.log(li);
 
 // 2. nav 클릭시 배경 색상 변경
 function setBgColor(index){
@@ -43,14 +46,24 @@ function setNameText(index) {
 	nickName.textContent = `${name}`;
 }
 
+// click한 li에 is-active 클래스 추가
+function highlightItem(target){
+  li.forEach((item)=>{
+    removeClass(item, 'is-active');
+  })
+  addClass(target, 'is-active');
+}
+
 // li목록 click시 텍스트(nickName), 배경, 이미지 변경
 function changePoster(e){
   let target = e.target.closest('li'); 
   index = attr(target, 'data-index') - 1;
 
+  highlightItem(target);
   setBgColor(index);
   setImage(target, index);
   setNameText(index);
+
 }
 
 nav.addEventListener('click', changePoster);
