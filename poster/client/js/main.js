a715cbbf4af9300d5f0893db82846fb378cd0b51
shju0317 <shju0317@naver.com>
2023-07-23 20:28:06 +0900

    click시 nickName 텍스트 변경

--- poster/client/js/main.js
+++ poster/client/js/main.js
@@ -12,24 +12,43 @@ import { attr, getNode } from '../lib/index.js'
 
 const nav = getNode('.nav')
 const visual = getNode('.visual')
+const nickName = getNode('.nickName')
+
+let index = 0;
 
 console.log("nav: ", nav);
 console.log("visual: ", visual);
 
-// 3. 이미지 변경
-function changeVisual(target){
+// 2. nav 클릭시 배경 색상 변경
+function changeBgColor(){
+  
+}
 
-  let index = attr(target, 'data-index') - 1;
+// 3. 이미지 변경
+function changeVisual(target, index){
+
+  
   let selectedImg = visual.querySelector('img');
   let {alt, name} = data[index];
 
   attr(selectedImg, 'src', `./assets/${name}.jpeg`);
 }
 
+// 4. 텍스트 변경
+function changeNickName(index) {
+	const {name} = data[index];
+
+	nickName.textContent = `${name}`;
+}
+
 function changePoster(e){
   let target = e.target.closest('li'); 
+  index = attr(target, 'data-index') - 1;
 
-  changeVisual(target);
+  changeVisual(target,index);
+  changeNickName(index);
+
+
 }
 
 nav.addEventListener('click', changePoster);
